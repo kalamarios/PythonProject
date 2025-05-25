3028652eb1fe2b57606117c30d9d2e3e2d9d3e38
kalamarios <marioskkts@gmail.com>
2025-05-25 15:08:34 +0300

    made a function named scrape_data for the web scraping and exporting to csv file

--- crypto.py
+++ crypto.py
@@ -1,33 +1,36 @@
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
+def scrape_data():
+    from bs4 import BeautifulSoup
+    import requests
+    import pandas as pd
 
-url = 'https://www.coinlore.com/'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
+    url = 'https://www.coinlore.com/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-titles = soup.find_all('th')
-table_titles = [title.text.strip() for title in titles ]
-table_titles = list(filter(None, table_titles))
-if table_titles:
-    table_titles = table_titles[:-7]
+    titles = soup.find_all('th')
+    table_titles = [title.text.strip() for title in titles]
+    table_titles = list(filter(None, table_titles))
+    if table_titles:
+        table_titles = table_titles[:-7]
 
-df = pd.DataFrame(columns = table_titles)
+    df = pd.DataFrame(columns=table_titles)
+
+    column_data = soup.find_all('tr')
+
+    for row in column_data[1:11]:
+        row_data = row.find_all('td')
+        individual_row_data = [data.text.strip() for data in row_data]
+        individual_row_data = list(filter(None, individual_row_data))
+        if individual_row_data:
+            individual_row_data = individual_row_data[:-1]
+        length = len(df)
+        df.loc[length] = individual_row_data
+
+    print(df)
+    df.to_csv(r'C:\Users\mario\Desktop\PythonProject\crypto.csv', index=False)
 
 
-column_data = soup.find_all('tr')
-
-for row in column_data[1:11]:
-    row_data = row.find_all('td')
-    individual_row_data = [data.text.strip() for data in row_data]
-    individual_row_data = list(filter(None, individual_row_data))
-    if individual_row_data:
-        individual_row_data = individual_row_data[:-1]
-    length = len(df)
-    df.loc[length] = individual_row_data
-
-print(df)
-df.to_csv(r'C:\Users\mario\Desktop\PythonProject\crypto.csv', index = False)
+scrape_data()
 
 
 
